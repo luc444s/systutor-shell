a39e65516d1694935fa564830398994ba452b436
sihuen <sihuen8@gmail.com>
2026-09-20 02:58:38 -0500

    fix(ui): move toasts to top right

--- src/ui/toast.tsx
+++ src/ui/toast.tsx
@@ -3,7 +3,7 @@ import { Toaster as SonnerToaster } from "sonner";
 export function Toaster() {
   return (
     <SonnerToaster
-      position="bottom-right"
+      position="top-right"
       toastOptions={{
         classNames: {
           toast:
